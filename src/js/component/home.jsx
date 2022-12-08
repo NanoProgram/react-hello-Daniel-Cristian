300ca9e9fe29a0de6dae52d7e8e9b9b91aca1012
NanoProgram <danielj.perezd@gmail.com>
2022-12-08 18:09:03 +0000

    Render listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import NavBar from "./barra.jsx";
-import Centre from "./centre.jsx"
+import Centre from "./centre.jsx";
 
-let titleJumbo = "A Warmn Welcome"
+let titleJumbo = "A Warmn Welcome";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 import Footer from "./footer.jsx";
 
 const Home = () => {
-	return (
-	<NavBar/>	
-  	<Centre titleJumbo={titleJumbo}/>
-	);
+  return <>
+  <NavBar/>
+  <Centre titleJumbo={titleJumbo} />
+  <Footer/>
+  </>
 };
 
 export default Home;
