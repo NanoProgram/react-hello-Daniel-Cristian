a653aa86b7ceca34ffcfcf2ef8aab23bc7527871
cristian-cuevas <cuevasgarciascristian@gmail.com>
2023-02-17 22:22:41 +0000

    lista la props de cristian

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import NavBar from "./barra.jsx";
 import Centre from "./centre.jsx";
-
-//let titleJumbo = "A Warmn Welcome";
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import Footer from "./footer.jsx";
 
 const Home = () => {
-  return <>
-  <NavBar/>
-  <Centre title= "A Warmn Welcome" money ={10} />
-  <Footer/>
-  </>
+  return (
+    <>
+      <NavBar />
+      <Centre title="A Warm Welcome" money={10} description="cristian el maximo" />
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
+
